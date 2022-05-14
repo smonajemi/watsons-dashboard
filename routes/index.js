b5f9823e7f980f72812600d94dca9508b66431bc
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-05-14 02:14:22 -0400

    commit

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,10 @@ router.get('/reservations', function(req, res, next) {
   res.render('partials/reservations', { title:"Watson's Reservation",layout: 'main',  isReservation: true})
 })
 router.get('/qr', function(req, res, next) {
-  const urlAddress = req.protocol + '://' + req.get('host');
+  // const urlAddress = req.protocol + '://' + req.get('host');
+  const urlAddress = req.headers.host.includes('localhost') ? process.env.HOST_LOCAL : process.env.HOST_TEST 
   res.render('menu', { title:"Watson's Toronto", url: urlAddress})
+  res.json(req.url)
 })
 
 module.exports = router;
