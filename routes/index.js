07169a851c8ce2d3c08c9b11705e7f4516a39cc8
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-05-16 10:33:44 -0400

    commit

--- routes/index.js
+++ routes/index.js
@@ -43,21 +43,19 @@ if (req.file) {
 
 } 
 });
-
-// router.post('/', upload.single('file'), (req, res) => {
-//   const newMenu = new Menu({
-//     name: req.file.filename,
-//     description: req.file.fieldname,
-//     file: req.file.path
-//   })
-
-//   try {
-//     newMenu.save()
-//   } catch (error) {
-//     console.info(error)
-//     res.redirect('/dashboard')
-//   }
-// });
+/** RULES OF OUR API */
+router.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Origin', 'https://young-dawn-06072.herokuapp.com');
+      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+      return res.status(200).json({});
+  }
+  next();
+});
 
 
 module.exports = router;
