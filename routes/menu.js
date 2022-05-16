d5c9544d52e96d2c613b6de85fce6eab26fc6f3b
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-05-16 17:58:21 -0400

    s

--- routes/menu.js
+++ routes/menu.js
@@ -1,61 +1,63 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Menu = require('../modules/Menu')
-const upload = require('../middlewares/upload')
-const path = require('path');
+const Menu = require("../modules/Menu");
+const upload = require("../middlewares/upload");
+const path = require("path");
 let exit = Boolean(false);
 
 /* GET menu */
-router.get('/', function(req, res, next) {
-  // res.sendFile(path.join(__dirname, "/views/registerUser.html"));
-
-const options = {
-    root: path.join(__dirname.replace('routes', 'uploads'))
-};
-const fileName = 'watsonsToronto.pdf';
-res.sendFile(fileName, options, function (err) {
+router.get("/", (req, res, next) => {
+  const options = {
+    root: path.join(__dirname.replace("routes", "uploads")),
+  };
+  const fileName = "watsonsToronto.pdf";
+  res.sendFile(fileName, options, (err) => {
     if (err) {
-        next(err);
+      next(err);
     } else {
-        console.log('Sent:', fileName);
+      console.log("Sent:", fileName);
     }
+  });
 });
-})
 
-router.post("/", upload.single('file'), (req, res, next) => {
-  //test new post
-if (req.file) {
-  const newMenu = new Menu({
-    name: req.file.filename,
-    description: req.file.originalname,
-    file: req.file.path
-  })
-    newMenu.save(function(err) {
-      if (err !== null) {
-          console.log(err);
-          return res.status(404).json(err)
+router.post("/", upload.single("file"), async (req, res, next) => {
+  if (req.file) {
+    const newMenu = new Menu({
+      name: req.file.filename,
+      description: req.file.originalname,
+      file: req.file.path,
+    });
+    try {
+      await newMenu.save();
+      const options = {
+        root: path.join(__dirname.replace("routes", "uploads")),
+      };
+      const fileName = "watsonsToronto.pdf";
+      res.sendFile(fileName, options, (err) => {
+        if (err) {
+          next(err);
+        } else {
+          console.log("Sent:", fileName);
+        }
+      });
+    } catch (error) {
+      res.status(404).json(error);
     }
-});
-req.body = req.file
-const formFile = req.file;
-const dataReceived = "Your submission was received:<br/><br/>" +
-"Your File data was:" + JSON.stringify(formFile.filename) + ' <br/> created at: ' + newMenu.createAt;
-res.send(dataReceived);
-} else return res.redirect('dashboard')
+  }
 });
 
 /** RULES OF OUR API */
-router.use((req, res, next) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-      res.setHeader('Access-Control-Allow-Origin', 'https://young-dawn-06072.herokuapp.com');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-      return res.status(200).json({});
-  }
-  next();
-});
+// router.use((req, res, next) => {
+//   res.setHeader('Content-Type', 'application/json');
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+//   if (req.method === 'OPTIONS') {
+//       res.setHeader('Access-Control-Allow-Origin', 'https://young-dawn-06072.herokuapp.com');
+//       res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+//       return res.status(200).json({});
+//   }
+//   next();
+// });
 
 module.exports = router;
