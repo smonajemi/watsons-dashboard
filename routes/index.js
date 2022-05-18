828157c9fa95b4e07a919b774ea85bdcef43e1cc
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-05-17 23:05:05 -0400

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,60 @@
-var express = require('express');
-var router = express.Router();
-const path = require("path");
-const Menu = require("../modules/Menu");
-const upload = require("../middlewares/upload");
+var express = require('express')
+var router = express.Router()
+const path = require("path")
+const Menu = require("../modules/Menu")
+const upload = require("../middlewares/upload")
 require('dotenv/config')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('dashboard', { title:"Dashboard", isDash: true})
-  // res.redirect('back');
+  // res.redirect('back')
 })
 router.get("/menu", (req, res, next) => {
     const options = {
         root: path.join(__dirname.replace('routes', 'uploads'))
-    };
-    const fileName = 'watsonsToronto.pdf';
+    }
+    const fileName = 'watsonsToronto.pdf'
     res.status(200).sendFile(fileName, options, (err) => {
         err ? next(err) : next()
-    });
-});
+    })
+})
 router.get('*', function(req, res, next) {
   res.redirect('/')
 })
 
 router.post("/postMenu", upload.single("file"), async (req, res, next) => {
-  const formFile = req.file;
+  const formFile = req.file
   let dataReceived = ''
   try {
     const newMenu = new Menu({
       name: formFile.filename,
       description: "Watson's Toronto Menu - QR Code",
       file: formFile.path,
-    });
-    await newMenu.save()
+    })
+    newMenu.save()
   } catch (error) {
     dataReceived = "Your submission was not successful" +
     `<br/><br/><a class="btn" href="/"><button>Dashboard</button></a>`
-    res.send(dataReceived);
+    res.send(dataReceived)
   }
   dataReceived = "Your submission was successful" +
   `<br/><br/><a class="btn" href="/"><button>Dashboard</button></a>` + 
   "<br/><br/> You uploaded: " + JSON.stringify(formFile.originalname) +
   `<br/><br/><a class="btn" href="/menu" target="_blank"><button>View Uploaded Menu</button></a>`
-  res.send(dataReceived);
-});
+  res.send(dataReceived)
+})
 
 /** RULES OF OUR API */
 // router.use((req, res, next) => {
-//   res.setHeader('Content-Type', 'application/pdf');
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+//   res.setHeader('Content-Type', 'application/pdf')
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization')
 //   if (req.method === 'OPTIONS') {
-//       res.setHeader('Access-Control-Allow-Origin', process.env.HOST_DEV);
-//       res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-//       return res.status(200).json({});
+//       res.setHeader('Access-Control-Allow-Origin', process.env.HOST_DEV)
+//       res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+//       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers')
+//       return res.status(200).json({})
 //   }
-//   next();
-// });
-module.exports = router;
+//   next()
+// })
+module.exports = router
