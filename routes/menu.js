642766ed699920b57585b3c4c8805a5c84aa2cd0
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-05-17 20:19:37 -0400

    commot

--- routes/menu.js
+++ routes/menu.js
@@ -31,18 +31,18 @@ router.post("/", upload.single("file"), async (req, res, next) => {
   res.send(dataReceived);
 });
 
-// router.use((req, res, next) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
-//   if (req.method === 'OPTIONS') {
-//       res.setHeader('Access-Control-Allow-Origin', process.env.HOST_DEV);
-//       res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-//       return res.status(200).json({});
-//   }
-//   next();
-// });
+router.use((req, res, next) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Origin', process.env.HOST_DEV);
+      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+      return res.status(200).json({});
+  }
+  next();
+});
 
 
 module.exports = router;
