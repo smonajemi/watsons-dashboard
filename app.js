8b652794285b5185179138d24cdb82612726fc67
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-05-18 00:04:58 -0400

    commit

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express();
 const http = require("http");
 const https = require("https");
 require('dotenv').config()
-const HTTP_PORT = process.env.PORT || 3000;
+const HTTP_PORT = process.env.HTTP-PORT || 3000;
+const HTTPS_PORT = process.env.HTTPS-PORT || 3000;
 // const https = require('https')
 // require('dotenv').config()
 // const HTTP_PORT = process.env.HTTP_PORT || 3000;
