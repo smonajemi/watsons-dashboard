24e6caa7aef2aeb47f144f68ca8295c3b26aef0a
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-05-19 20:01:59 -0400

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,64 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../modules/User')
+const bcrypt = require('bcrypt');
 /* GET users listing. */
+const saltRounds = 10; 
 
 router.post('/', (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    role: req.body.role,
-    password: req.body.password
-  })
-  newUser.save().then(data => {
-    res.json(data)
-  }).catch(e => {
-    res.json({message: e})
-  })
+const password = req.body.password
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) return res.send({err})
+    bcrypt.hash(password, salt, (err, hash) => {
+      if (err) return res.send({err})
+      const newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        role: req.body.role,
+        password: hash
+      })
+      newUser.save().then(data => {
+        res.json(data)
+      }).catch(e => {
+        res.json({message: e})
+      })
+    });
+  });
+  
+
 });
 
 module.exports = router;
+
+
+
+
+// var express = require("express");
+// var router = express.Router();
+// const User = require("../modules/User");
+// const bcrypt = require('bcrypt');
+// /* GET users listing. */
+
+// router.post("/", (req, res) => {
+//   bcrypt.hash(req.body.password, salt, (err, hash) => {
+//     if (err) return next(err);
+//     const newUser = new User({
+//       username: req.body.username,
+//       firstName: req.body.firstName,
+//       lastName: req.body.lastName,
+//       role: req.body.role,
+//       password: hash,
+//     });
+//     newUser
+//       .save()
+//       .then((data) => {
+//         res.json(data);
+//       })
+//       .catch((e) => {
+//         res.json({ message: e });
+//       });
+//   });
+// });
+// module.exports = router;
+
