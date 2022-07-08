23ff6f9a035b9c09bbb1bce6df248f3995a189fb
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-07-08 03:46:12 -0400

    session duration extended

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(clientSessions({
   cookieName: "session", // this is the object name that will be added to 'req'
   secret: process.env.SESSION_SECRET_WORD, // this should be a long un-guessable string.
-  duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+  duration: 15 * 60 * 1000, // duration of the session in milliseconds (15 minutes)
   activeDuration: 1000 * 60 // the session will be extended by this many ms each request (1 minute)
 }))
 
