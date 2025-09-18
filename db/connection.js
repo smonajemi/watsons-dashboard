ba88967f698bb5ce757314b1abfea817a107dccb
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2025-09-18 17:12:49 -0400

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,28 +2,18 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 
 const connectDB = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        await mongoose.connect(process.env.DB_CONNECTION, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-        console.log("Connected to Mongoose");
-    } catch (error) {
-        console.log("Error connecting to Mongoose:", error.message);    
-    }
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000 // 20s timeout
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1); // optional: crash the app if no DB
+  }
 };
 
-module.exports = connectDB;
-
-
-// let gfs, gridfsBucket
-// const conn = mongoose.connection;
-// conn.once('open', () => {
-//     gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
-//         bucketName: 'menus'
-//     });
-//     gfs = Grid(conn.db, mongoose.mongo);
-//     gfs.collection('menus');
-// })
 module.exports = connectDB;
